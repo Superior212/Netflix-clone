6ea80a62a3dec62c9be8fb250f61ac73e1244fcb
Samson Aderonmu <94833173+Superior212@users.noreply.github.com>
2023-01-12 19:08:13 +0100

    added javascript to the profile page

---
+++ script.js
@@ -0,0 +1,26 @@
+const users = ["Murat","Umut","Kemal","Çocuk"];
+
+const memberDiv = document.querySelector(".memberDiv");
+const addIcon = document.querySelector(".addIcon");
+
+const  userIcons = () =>{
+    users.map((currentElement)=>{
+        users.reverse()
+        memberDiv.insertAdjacentHTML("afterbegin",`
+        <button class="btn"><span>${currentElement}</span></button>
+        `);
+    })
+}
+addIcon.addEventListener("click", ()=>{
+    let userName = prompt("Please enter your name");
+    if (userName !== null && !users.includes(userName)) {
+        users.push(userName)
+        console.log(users);
+        memberDiv.insertAdjacentHTML("afterbegin",`
+        <button class="btn"><span>${userName}</span></button>
+        `);
+    }else{
+        alert("username already exist");
+    }
+})
+userIcons();
